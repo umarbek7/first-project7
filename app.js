4c2201c83c55ab01f41cb8c95fccc580ecbc2b06
umarbek7 <umarbekzakirov73@gmail.com>
2024-07-26 01:54:47 +0500

    proyektimiz ajoyib ketmoqda js ni ham ulab qoydik

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const number1 = document.getElementById(num1);
 const number2 = document.getElementById(num2);
 
-alert(number1)
-alert(number2)
+const p1 = document.getElementById("result1");
+const p2 = document.getElementById("result2");
+const p3 = document.getElementById("result3");
+const p4 = document.getElementById("result4");
